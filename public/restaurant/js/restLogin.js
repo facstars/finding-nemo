ca82cd9641d74b5d74715a69d4a4197717b57cb9
Marziyeh Ahmed <marziyeh.naaz.ahmed@gmail.com>
2016-01-21 16:58:01 +0000

    restaurant login page working fixed #25 #26

---
+++ public/restaurant/js/restLogin.js
@@ -0,0 +1,21 @@
+var ref = new Firebase("https://blistering-torch-1660.firebaseio.com");
+
+document.getElementById('restLogin').addEventListener('submit', function(e){
+  e.preventDefault();
+  var email=restLogin.elements.email.value;
+  var pw=restLogin.elements.password.value;
+  validateRestLoginDetails(email,pw);
+})
+
+var validateRestLoginDetails = function(email,pw){
+  ref.authWithPassword({
+    email    : email,
+    password : pw
+  }, function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+    }
+  });
+};
