3e62a4ceffab1f0c8c91213b4500826c7c591f79
Marziyeh Ahmed <marziyeh.naaz.ahmed@gmail.com>
2016-01-21 16:35:39 +0000

    restaurant sign up fix #45

--- public/restaurant/js/restValidate.js
+++ public/restaurant/js/restValidate.js
@@ -1,21 +1,46 @@
-console.log('hey mate');
-
+var ref = new Firebase("https://blistering-torch-1660.firebaseio.com");
 
 document.getElementById('newRest').addEventListener('submit', function(e){
   e.preventDefault();
-  var array = ["email", "password", "restName", "address", "description", "image", "openTime", "closeTime"];
-  var newRestInputObj = array.reduce(function(accum, elem){
+  var AuthDetailsArray = ["email", "password"];
+  var newRestAuthDetailsObj = AuthDetailsArray.reduce(function(accum, elem){
     accum[elem] = newRest.elements[elem].value;
     return accum;
   }, {});
+  setUserAuth(newRestAuthDetailsObj);
+});
 
-  var ref = new Firebase("https://blistering-torch-1660.firebaseio.com");
-  ref.createUser(newRestInputObj, function(error, userData){
+
+var setUserAuth = function (obj){
+  ref.createUser(obj, function(error, userData){
     if(error){
       console.log("error creating user", error);
     }
     else {
-      console.log("Successfully created user account with uid:" + userData);
+      console.log("Successfully created user account with uid:" + userData.uid);
+      setUserDetails(userData.uid);
     }
-  });
-});
+  })
+}
+
+var setUserDetails = function(restUserId){
+  var userDetails = new Firebase("https://blistering-torch-1660.firebaseio.com/"+restUserId);
+  var RestDetailsArray = ["restName", "address", "description", "image", "openTime", "closeTime"];
+  var newRestDetailsObj = RestDetailsArray.reduce(function(accum, elem){
+    accum[elem] = newRest.elements[elem].value;
+    return accum;
+  }, {});
+
+  userDetails.set(
+    newRestDetailsObj
+    ,onComplete);
+};
+
+
+var onComplete = function(error) {
+  if (error) {
+    console.log(error);
+      } else {
+          console.log('Success, Details have been added to the database');
+      }
+};
