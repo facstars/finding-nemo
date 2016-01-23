c8665beb034b063ab779e437b6d06fbea9000ba4
Justen <justenbarget@gmail.com>
2016-01-23 12:24:16 +0000

    fixes #32 added html for modal, when save button clicked new anonymous user created but not yet added to waitlist #33 #34

--- public/restaurant/js/rest.js
+++ public/restaurant/js/rest.js
@@ -14,8 +14,7 @@ var loadRestDetails =(function(){
 }) ();
 
 function loadNumPeopleWaiting(tableNo){
-  var restaurantWaitlist=new Firebase("https://blistering-torch-1660.firebaseio.com/restaurants/"+ruid+"/waitlist/table"+tableNo);
-
+  var restaurantWaitlist = new Firebase("https://blistering-torch-1660.firebaseio.com/restaurants/"+ruid+"/waitlist/table"+tableNo);
     restaurantWaitlist.on('value', function(snapshot) {
       var numTableObj=snapshot.val();
       document.getElementById("numPeopleWaitingTable"+tableNo).innerHTML=numTableObj.length-1;
@@ -24,4 +23,29 @@ function loadNumPeopleWaiting(tableNo){
 
 var errorHandler = function(errorObject) {
   console.log("The read failed: " + errorObject.code);
+};
+
+  document.getElementById('modalForm').addEventListener('submit', function(e){
+    var tempUser = new Firebase ("https://blistering-torch-1660.firebaseio.com/users/");
+    e.preventDefault();
+    tempUser.authAnonymously(function(error, authData, modalForm) {
+      console.log(modalForm);
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData.uid);
+        // addTempUserToWaitlist(authData);
+        // var tempUserDetails = ["name", "phone-number", "table-size"];
+        // var tempUserDetailsObj = tempUserDetails.reduce(function(obj, detail){
+        //   obj[detail] = modalForm[detail].value;
+        //   return obj;
+        // }, {});
+        // addTempUserToWaitlist(authData, tempUserDetailsObj, ruid);
+      }
+    });
+  });
+
+var addTempUserToWaitlist = function(authData){
+  console.log(authData.uid);
+  // restaurantWaitlist.update();
 };
