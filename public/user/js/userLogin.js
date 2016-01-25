e00bc6add378d85e8ef4b8b6f72ce75e0c0416ad
katkelemen <kelemenkata@gmail.com>
2016-01-25 23:28:17 +0000

    #66 good login gets firebase_token and creates a cookie

--- public/user/js/userLogin.js
+++ public/user/js/userLogin.js
@@ -1,23 +1,9 @@
 var ref = new Firebase("https://blistering-torch-1660.firebaseio.com/users");
 
-document.getElementById('user-login').addEventListener('submit', function(e){
-  e.preventDefault();
-  var tel=userLogin.elements.tel.value;
-  var pw=userLogin.elements.password.value;
-  validateRestLoginDetails(tel,pw);
+ref.authWithCustomToken($.cookie("firebase_token"), function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Login Succeeded!", authData);
+  }
 });
-
-var validateRestLoginDetails = function(phone_number,pw){
-  ref.authWithPassword({
-    tel      : tel,
-    password : pw
-  }, function(error, authData) {
-    if (error) {
-      console.log("Login Failed!", error);
-    } else {
-      console.log("Authenticated successfully with payload:", authData);
-      localStorage.setItem('RUID',authData.auth.uid);
-      window.location.replace('../availableRestaurants');
-    }
-  });
-};
