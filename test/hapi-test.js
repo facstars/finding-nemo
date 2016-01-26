a4bcba405d4e10c67b60e80101d1c928535ae1a5
Marziyeh Ahmed <marziyeh.naaz.ahmed@gmail.com>
2016-01-26 13:37:55 +0000

    hapi tests added and precommit added fixed #74

---
+++ test/hapi-test.js
@@ -0,0 +1,27 @@
+var test    = require('tape');
+var path    = require('path');
+
+var dir     = __dirname.split('/')[__dirname.split('/').length-1];
+var file    = dir + __filename.replace(__dirname, '') + ' ->';
+
+var start = require('../lib/start.js');
+var server   = require('../lib/index.js');
+
+var testEndPoint = function(endpoint){
+  test(file + " GET "+ endpoint +" returns status 200", function(t) {
+    var options = {
+      method  : "GET",
+      url     : endpoint
+    };
+    server.inject(options, function (res) {
+      t.equal(res.statusCode, 200, 'server loads ok');
+        server.stop(t.end);
+    });
+  });
+};
+
+testEndPoint("/");
+testEndPoint("/restaurantSpecific/table2");
+testEndPoint("/restaurant/restOverview.html");
+testEndPoint("/user/index.html");
+testEndPoint("/css/public.css");
