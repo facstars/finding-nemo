d2d015b7ba345ba1e22e67760ff0dc1738999533
Marziyeh Ahmed <marziyeh.naaz.ahmed@gmail.com>
2016-01-28 15:32:05 +0000

    firebase token created when user signs up fixed 128 and restaurant details appears on checkIn page

--- public/user/js/checkIn.js
+++ public/user/js/checkIn.js
@@ -1,13 +1,29 @@
 var uid = localStorage.getItem('UID');
-
-console.log(uid);
 var ruid;
+
 
 var getRUID = (function(){
   var URLSegmentArray = window.location.pathname.split( '/' );
   ruid = URLSegmentArray[URLSegmentArray.length - 1];
   console.log("RUID" , ruid);
+  getRestDetailsObj(displayRestDetails);
 })();
+
+function getRestDetailsObj(callback){
+  var restDetails= new Firebase("https://blistering-torch-1660.firebaseio.com/restaurants/"+ruid);
+  restDetails.on("value", function(snapshot){
+    var restDetailsObj=snapshot.val();
+    callback(restDetailsObj);
+    // return restDetailsObj;
+  });
+}
+
+function displayRestDetails(restDetailsObj){
+  var html = "<div class='restDetailWrapper'><img class='restLogo' src=" + restDetailsObj.image +"><p class='restTimes'>"+restDetailsObj.openTime+ " - " +restDetailsObj.closeTime+ "</p><h3 class='restName'>"+restDetailsObj.restName+"</h3><p class='restDescription'>"+restDetailsObj.description+"</p><p class='restAddress'>"+restDetailsObj.address+"</p></div>";
+  document.getElementById("restDetails").innerHTML=html;
+
+}
+
 
 document.getElementById('checkInForm').addEventListener('submit', function(e){
   e.preventDefault();
@@ -16,13 +32,12 @@ document.getElementById('checkInForm').addEventListener('submit', function(e){
 
   User.on("value", function(snapshot){
     var userDetailsObj=snapshot.val();
+    console.log(userDetailsObj);
     var userBookingDetailsObj = {name:userDetailsObj.name, tel: userDetailsObj.tel, guests:checkInForm.guests.value};
     console.log(userBookingDetailsObj);
     addUserToWaitlist(userBookingDetailsObj);
   });
-
 });
-
 
 var addUserToWaitlist = function(bookingObj){
   var tableNo = bookingObj.guests > 4 ? "5" : bookingObj.guests > 2 ? "4" : "2";
