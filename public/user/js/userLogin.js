5a5a4c0522919ddaf633e8000aa6a718aefab8b1
katkelemen <kelemenkata@gmail.com>
2016-01-29 13:11:25 +0000

    #133 jquery added to signup fields for confirm password functionality

--- public/user/js/userLogin.js
+++ public/user/js/userLogin.js
@@ -16,3 +16,14 @@ var pwerror = getUrlVars().error;
 if (pwerror === "password") {
   $("#incorrect-p")[0].innerHTML = "Incorrect phone number or password!";
 }
+
+if (pwerror === "confirmPassword") {
+  $("#create #password")[0].style.borderColor = "#E34234";
+  $("#confirm-password")[0].style.borderColor = "#E34234";
+  $("#incorrect-confirm")[0].innerHTML = "Passwords do not match!";
+  $("#create, #login").toggleClass("fade");
+  $("#create, #login").toggleClass("active");
+  $(".nav-tabs>li").toggleClass("active");
+
+
+}
