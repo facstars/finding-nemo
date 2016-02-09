0bd06e388dbb4f3abc641131fdf100761831e527
Justen <justenbarget@gmail.com>
2016-02-09 15:55:05 +0000

    fixes #263, removing 'redirect' from userSignup reply

--- public/user/js/userLogin.js
+++ public/user/js/userLogin.js
@@ -17,8 +17,7 @@ function getUrlVars()
 {
     var vars = [], hash;
     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
+    for (var i = 0; i < hashes.length; i++) {
         hash = hashes[i].split('=');
         vars.push(hash[0]);
         vars[hash[0]] = hash[1];
